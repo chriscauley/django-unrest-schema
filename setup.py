8b79266e2e693c73e7ce002b9862ba438f6675b3
chriscauley <chris@lablackey.com>
2022-07-23 12:26:02 +0000

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'django-unrest-schema',
   packages = find_packages(),
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'A library for serving django forms using json schema and a restful api.',
   long_description="",
   long_description_content_type="text/markdown",
