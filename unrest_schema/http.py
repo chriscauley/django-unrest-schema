742597532424f41ac0b5b41e8b4893a3709db039
chriscauley <379151+chriscauley@users.noreply.github.com>
2025-02-07 10:50:20 -0500

    adding Point to JsonEncoder

--- unrest_schema/http.py
+++ unrest_schema/http.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
 from django.forms.models import ModelChoiceIteratorValue
 from django.http import JsonResponse as _JsonResponse
+from django.contrib.gis.geos import Point
 
 class JsonEncoder(DjangoJSONEncoder):
     def default(self, obj):
@@ -9,6 +10,8 @@ class JsonEncoder(DjangoJSONEncoder):
             return obj.value
         if obj.__class__.__name__ in getattr(settings, 'UNREST_STRING_TYPES', []):
             return str(obj)
+        if isinstance(obj, Point):
+            return list(obj)
         return super().default(obj)
 
 def JsonResponse(*args, **kwargs):
